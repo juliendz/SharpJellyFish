f9ea140c5ec73c41f6133396f87230c2593bb233
Julien Dcruz <juliendcruz@gmail.com>
2021-06-09 19:26:35 +0530

    First commit

---
+++ README.rst
@@ -0,0 +1,33 @@
+SharpJellyFish: JellyFish for .Net(3.5)
+==============================
+
+This is a port of Jellyfish (https://github.com/jamesturk/jellyfish) to the C# programming language. 
+
+Supported framework versions:
+------------------------------------
+.Net 3.5
+
+Planned framework versions:
+------------------------------------
+.Net 5
+
+
+Algorithms ported so far:
+------------------------------------
+- Phonetic encoding:
+    - American Soundex [X]
+    - Metaphone [ ]
+    - NYSIIS (New York State Identification and Intelligence System) [ ]
+    - Match Rating Codex [ ]
+
+
+- String comparison:
+    - Levenshtein Distance [ ]
+    - Damerau-Levenshtein Distance [ ]
+    - Jaro Distance [ ]
+    - Jaro-Winkler Distance [ ]
+    - Match Rating Approach Comparison [ ]
+    - Hamming Distance [ ]
+
+
+
---
+++ src/net3.5/SharpJellyFish/SharpJellyFish.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SharpJellyFish
+{
+    public class SharpJellyFish
+    {
+        private string Normalize(string s)
+        {
+            return s.Normalize(NormalizationForm.FormKD);
+        }
+
+        public string SoundEx(string input)
+        {
+
+            List<string[]> _replacements = new List<string[]>()
+            {
+                new string [2] {"BFPV", "1"},
+                new string [2] {"CGJKQSXZ", "2"},
+                new string [2] {"DT", "3"},
+                new string [2] {"L", "4"},
+                new string [2] {"MN", "5"},
+                new string [2] {"R", "6"},
+            };
+
+
+            if (string.IsNullOrEmpty(input))
+            {
+                return "";
+            }
+
+            var s = Normalize(input);
+            s = s.ToUpper();
+
+            var result = new List<char>();
+            result.Add(s[0]);
+            var count = 1;
+
+            var last = "";
+            // find would-be replacement for first character
+            foreach (var replacementStr in _replacements)
+            {
+                var lset = replacementStr[0];
+                var sub = replacementStr[1];
+                if (lset.Contains(s[0]))
+                {
+                    last = sub;
+                    break;
+                }
+            }
+
+            foreach (var letter in s.ToList().GetRange(1, s.Count() - 1))
+            {
+                var isLetterExistInReplacments = false;
+                foreach (var replacementStr in _replacements)
+                {
+                    var lset = replacementStr[0];
+                    var sub = replacementStr[1];
+                    if (lset.Contains(letter))
+                    {
+                        if (sub != last)
+                        {
+                            result.Add(sub.ToCharArray()[0]);
+                            count++;
+                        }
+                        last = sub;
+                        isLetterExistInReplacments = true;
+                        break;
+                    }
+                }
+
+                if (!isLetterExistInReplacments)
+                {
+                    if (letter != 'H' && letter != 'W')
+                    {
+                        last = "";
+                    }
+                }
+
+                if (count == 4)
+                    break;
+            }
+
+            var finalResult = new string(result.ToArray());
+            //Pad the string with extra zeros to make it max of 4 letters
+            for (var i = 0; i < (4 - count); i++)
+            {
+                finalResult += "0";
+            }
+
+            return finalResult;
+        }
+    }
+}
